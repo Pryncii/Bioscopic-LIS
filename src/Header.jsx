b525a1a2e5217351cda595134727192fc00458b8
RocketRoooiz <chester_ruiz@dlsu.edu.ph>
2024-09-24 19:00:14 +0800

    Header component
    things kinda wonk when window is small and you slide to the right

---
+++ src/Header.jsx
@@ -0,0 +1,34 @@
+import './Header.css'
+import logoPic from './assets/header_logo.png'
+import userPic from './assets/default_icon.webp'
+
+function Header() {
+    
+    return (
+      <div className = "headerBar">
+        <img className = "headerLogoPic" src = {logoPic} alt="Laboratory Logo"></img> 
+        
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Add Patient Data</a></li>
+            <li><a href="/">Add Patient Request</a></li>
+            <li><a href="/">View All Patients</a></li>
+            <li className="dropdown">
+              <a className="dropdown-toggle" id="headerDropdown" role="button" data-bs-toggle="dropdown">
+                <img className="headerUserPic" src={userPic} alt="Profile Picture" />
+              </a>
+              <div className="dropdown-menu" aria-labelledby="headerDropdown">
+                <h6 className="dropdown-header">Hello, Name!</h6>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="/">View Profile</a>
+                <a className="dropdown-item" href="/">Log Out</a>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+  
+  export default Header
