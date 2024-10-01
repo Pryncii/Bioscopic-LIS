27e5c18d629477f8b4fe7448cdcb439c92046336
Pryncii <princebuencamino@gmail.com>
2024-10-01 23:29:39 +0800

    setup mongodb connection

---
+++ Server/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect ('mongodb+srv://princebuencamino:LIS092901@lis.1ioj1.mongodb.net/?retryWrites=true&w=majority&appName=LIS');
+        console.log('MongoDB connected:')
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB
---
+++ Server/index.js
@@ -0,0 +1,7 @@
+// mongodb+srv://princebuencamino:LIS092901@lis.1ioj1.mongodb.net/?retryWrites=true&w=majority&appName=LIS
+
+const express = require ('express')
+const connectDB = require('./db.js')
+const app = express()
+connectDB()
+
