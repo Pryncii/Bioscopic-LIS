c53f9e4f42317b4a2e54f1e1cba038590a399a4e
RocketRoooiz <chester_ruiz@dlsu.edu.ph>
2024-10-16 18:14:14 +0800

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Bioscopic-LIS/',
+  server: {
+    port: process.env.PORT || 3000, // Use the port from the environment variable or default to 3000
+    strictPort: true // Prevents Vite from trying to use another port if the specified one is taken
+  },
+  build: {
+    outDir: 'dist', // Specify the output directory
+  },
 })
