e5b73c71f4799a5201ec36af260ab4c316bc0d77
RocketRoooiz <chester_ruiz@dlsu.edu.ph>
2024-10-16 18:21:42 +0800

    rendering a different page since the login page doesnt seem to work

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App.jsx';
-import Login from './Login.jsx';
+import ViewPatientHistory from './ViewPatientHistory.jsx'; // for testing
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Login />
+    <ViewPatientHistory />
 );
