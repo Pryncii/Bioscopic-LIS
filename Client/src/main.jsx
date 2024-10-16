f09c624320a0565d7bc7f4bdae338a71d27683dd
RocketRoooiz <chester_ruiz@dlsu.edu.ph>
2024-10-16 17:21:28 +0800

    trying render

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App.jsx';
-import ViewPatientHistory from './ViewPatientHistory.jsx'; // for testing
+import Login from './Login.jsx';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <ViewPatientHistory />
+    <Login />
 );
