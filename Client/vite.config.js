f2605f430653ae34907ce6b4398669c67c7c4984
RocketRoooiz <chester_ruiz@dlsu.edu.ph>
2024-10-16 18:43:06 +0800

    still trying

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT || 3000, // Use the port from the environment variable or default to 3000
-    strictPort: true // Prevents Vite from trying to use another port if the specified one is taken
+    host: '0.0.0.0'
   },
   build: {
     outDir: 'dist', // Specify the output directory
