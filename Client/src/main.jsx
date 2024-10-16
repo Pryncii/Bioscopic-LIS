a01fdfe9f99eb2b66a243b831116acac67a7ad65
RocketRoooiz <chester_ruiz@dlsu.edu.ph>
2024-10-16 16:39:01 +0800

    debugging

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App.jsx';
+import ViewPatientHistory from './ViewPatientHistory.jsx'; // for testing
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ViewPatientHistory />
 );
