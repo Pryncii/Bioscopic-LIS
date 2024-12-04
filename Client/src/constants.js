867ec6fc3c678f3ddb32dbec64605d3e749d4cef
RocketRoooiz <138289829+RocketRoooiz@users.noreply.github.com>
2024-12-04 13:33:49 +0800

    changed url for backend

--- Client/src/constants.js
+++ Client/src/constants.js
@@ -1,4 +1,5 @@
 // constants.js
-export const URL = 'http://localhost:4000';
+//export const URL = 'http://localhost:4000';
 //export const URL = 'https://bioscopic-lis.onrender.com';
+export const URL = 'https://bioscopic-lis-backend.onrender.com';
 
